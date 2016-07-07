6b72aa745e3a29dfc36055c392e8b4ca2b9ed9f5
groovecoder <luke.crouch@gmail.com>
2016-07-07 15:27:51 -0500

    for #2: 3.9/4 SafeBrowsing IP host canonicalization

--- js/background.js
+++ js/background.js
@@ -1,6 +1,10 @@
 var TESTPILOT_TELEMETRY_CHANNEL = 'testpilot-telemetry';
 var testpilotPingChannel = new BroadcastChannel(TESTPILOT_TELEMETRY_CHANNEL);
 
+var ip4DecimalPattern = '^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?))$';
+var ip4HexPattern = '^(?:(?:0x[0-9a-f]{1,2})\.){3}(?:0x[0-9a-f]{1,2})$';
+var ip4OctalPattern = '^(?:(?:03[1-7][0-7]|0[12][0-7]{1,2}|[0-7]{1,2})\.){3}(?:03[1-7][0-7]|0[12][0-7]{1,2}|[0-7]{1,2})$';
+
 var blocklist = {};
 var allowedHosts = [];
 
@@ -37,10 +41,24 @@ function canonicalizeHost(host) {
   // Replace consecutive dots with a single dot
   canonicalizedHost = canonicalizedHost.replace(new RegExp('[\.+]'), '.');
 
-  // TODO: If the hostname can be parsed as an IP address,
+  // If the hostname can be parsed as an IP address,
   // normalize it to 4 dot-separated decimal values.
   // The client should handle any legal IP-address encoding,
-  // including octal, hex, and fewer than four components
+  // including octal, hex, and TODO: fewer than four components
+  var isIP4Decimal = isIP4Hex = isIP4Octal = false;
+  var base = 10;
+
+  isIP4Decimal = canonicalizedHost.match(ip4DecimalPattern) != null;
+  isIP4Hex = canonicalizedHost.match(ip4HexPattern) != null;
+  isIP4Octal = canonicalizedHost.match(ip4OctalPattern) != null;
+  if (isIP4Hex || isIP4Octal) {
+    if (isIP4Hex) {
+      base = 16;
+    } else if (isIP4Octal) {
+      base = 8;
+    }
+    canonicalizedHost = canonicalizedHost.split('.').map(num => parseInt(num, base)).join('.');
+  }
 
   // Lowercase the whole string
   canonicalizedHost = canonicalizedHost.toLowerCase();
