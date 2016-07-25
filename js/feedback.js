33f7f283f0deec1d5e80a4b1d4bb42fedaf5a636
Peter deHaan <peter@deseloper.com>
2016-07-25 11:35:08 -0700

    Switch from ESLint to Standard for code formatting

--- js/feedback.js
+++ js/feedback.js
@@ -1,14 +1,14 @@
 for (let closeBtn of document.querySelectorAll('.close-feedback')) {
   closeBtn.addEventListener('click', function () {
-    browser.runtime.sendMessage('close-feedback');
-  });
+    browser.runtime.sendMessage('close-feedback')
+  })
 }
 
-document.querySelector('.submit').addEventListener('click', function() {
+document.querySelector('.submit').addEventListener('click', function () {
   let message = {
     'breakage': document.querySelector('input:checked').value,
     'notes': document.querySelector('textarea#notes').textContent
-  };
-  browser.runtime.sendMessage(message);
-  browser.runtime.sendMessage('close-feedback');
-});
+  }
+  browser.runtime.sendMessage(message)
+  browser.runtime.sendMessage('close-feedback')
+})
--- js/toolbar.js
+++ js/toolbar.js
@@ -1,42 +1,42 @@
 browser.runtime.onMessage.addListener(function (runtimeMessage) {
-  var blockedEntitiesCount, allowedEntitiesCount;
+  var blockedEntitiesCount, allowedEntitiesCount
   if (runtimeMessage.hasOwnProperty('origin-disabled')) {
-    allowedEntitiesCount = runtimeMessage.allowed_entities.length;
+    allowedEntitiesCount = runtimeMessage.allowed_entities.length
 
-    document.querySelector('#title-blocking').className = 'hide';
-    document.querySelector('#title-disabled').className = 'title';
-    document.querySelector('#title-allowed-count').innerHTML = allowedEntitiesCount;
+    document.querySelector('#title-blocking').className = 'hide'
+    document.querySelector('#title-disabled').className = 'title'
+    document.querySelector('#title-allowed-count').innerHTML = allowedEntitiesCount
 
     for (let feedbackElement of document.querySelectorAll('.feedback')) {
-      feedbackElement.className = 'feedback hide';
+      feedbackElement.className = 'feedback hide'
     }
 
     if (runtimeMessage.hasOwnProperty('reason-given') && runtimeMessage['reason-given'] != null) {
-      document.querySelector('#disable-reason-thankyou').className = '';
+      document.querySelector('#disable-reason-thankyou').className = ''
     } else {
-      document.querySelector('#disable-reasons').className = '';
+      document.querySelector('#disable-reasons').className = ''
     }
   } else {
-    blockedEntitiesCount = runtimeMessage.blocked_entities.length;
-    document.querySelector('#title-block-count').innerHTML = blockedEntitiesCount;
+    blockedEntitiesCount = runtimeMessage.blocked_entities.length
+    document.querySelector('#title-block-count').innerHTML = blockedEntitiesCount
   }
-});
+})
 
 document.querySelector('#disable-link').addEventListener('click', function () {
-  browser.runtime.sendMessage("disable");
-});
+  browser.runtime.sendMessage('disable')
+})
 
 document.querySelector('#re-enable-link').addEventListener('click', function () {
-  browser.runtime.sendMessage("re-enable");
-});
+  browser.runtime.sendMessage('re-enable')
+})
 
 for (let feedbackBtn of document.querySelectorAll('.feedback-btn')) {
   feedbackBtn.addEventListener('click', function (event) {
-    var feedback = event.target.dataset.feedback;
-    browser.runtime.sendMessage({"feedback": feedback});
+    var feedback = event.target.dataset.feedback
+    browser.runtime.sendMessage({'feedback': feedback})
     for (let feedbackDiv of document.querySelectorAll('.feedback')) {
-      feedbackDiv.className = 'hide';
+      feedbackDiv.className = 'hide'
     }
-    document.querySelector('#feedback-' + feedback).className = 'feedback';
-  });
+    document.querySelector('#feedback-' + feedback).className = 'feedback'
+  })
 }
