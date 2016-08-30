8bc629c53da3be6b19b5adc6ed751f376c11f762
groovecoder <luke.crouch@gmail.com>
2016-08-30 10:17:25 -0500

    use JSX for components

--- js/popup.js
+++ js/popup.js
@@ -1,36 +1,34 @@
-const {DOM, createFactory, createClass, createElement} = require('react')
-const {div, a, span, input, label, hr, h5, p, fieldset, legend, textarea} = DOM
+const React = require('react')
 const ReactDOM = require('react-dom')
 
 let disabled = false
-let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
-let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
 
-const Title = createFactory(createClass({
+const Title = React.createFactory(React.createClass({
   displayName: 'Title',
 
   render: function () {
-    let {disabled} = this.props
     let titleText = 'Blok is blocking tracker requests on this site.'
-    if (disabled) {
+    if (this.props.disabled) {
       titleText = 'Parts of this site may be tracking your activity.'
     }
 
     return (
-      div({ className: 'columns' },
-        div({ className: 'title' },
-          titleText
-        )
-      )
+      <div className='columns'>
+        <div className='title'>
+          {titleText}
+        </div>
+      </div>
     )
   }
 }))
 
-const HostReport = createFactory(createClass({
+const HostReport = React.createFactory(React.createClass({
   displayName: 'Host Report',
 
   render: function () {
     const {hostReport} = this.props
+    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+    const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
     const date = new Date(hostReport.dateTime)
     const hostReportDateTimeString = days[date.getDay()] + ', ' + months[date.getMonth()] + ' ' + date.getDate()
 
@@ -45,14 +43,12 @@ const HostReport = createFactory(createClass({
     reportText += hostReportDateTimeString + '.'
 
     return (
-      div({ className: 'columns' },
-        reportText
-      )
+      <div className='columns'>{reportText}</div>
     )
   }
 }))
 
-const Feedback = createFactory(createClass({
+const Feedback = React.createFactory(React.createClass({
   feedbackBtnHandler: function (event) {
     let feedback = event.target.dataset.feedback
     browser.runtime.sendMessage({'feedback': feedback})
@@ -65,183 +61,136 @@ const Feedback = createFactory(createClass({
 
   render: function () {
     return (
-      div({ className: 'columns' },
-        a(
-          {
-            className: 'feedback-btn secondary expanded button',
-            onClick: this.feedbackBtnHandler,
-            'data-feedback': 'page-works'
-          },
-          'This page works well'
-        ),
-        a(
-          {
-            className: 'feedback-btn expanded button',
-            onClick: this.feedbackBtnHandler,
-            'data-feedback': 'page-problem'
-          },
-          'Report a problem'
-        )
-      )
+      <div className='columns'>
+        <a className='feedback-btn secondary expanded button' onClick={this.feedbackBtnHandler} data-feedback='page-works'>
+          This page works well
+        </a>
+        <a className='feedback-btn expanded button' onClick={this.feedbackBtnHandler} data-feedback='page-problem'>
+          Report a problem
+        </a>
+      </div>
     )
   }
 }))
 
-const Toggle = createFactory(createClass({
-  /*
-  componentDidUpdate: function () {
-    if (this.props.disabled) {
-      this._input.removeAttribute('checked')
-    } else {
-      this._input.setAttribute('checked', true)
-    }
-  },
-  */
-
+const Toggle = React.createFactory(React.createClass({
   render: function () {
     let {disabled, sendToggleMessage} = this.props
     let currentStatus = disabled ? 'disabled' : 'enabled'
     let labelText = 'Blok is ' + currentStatus + ' for this site.'
+    let checked = disabled ? null : true
 
-    return div(
-      {className: 'row align-middle'},
-      div(
-        {className: 'small-8 columns toggle-label-column'},
-        labelText
-      ),
-      div(
-        {className: 'small-4 columns toggle-column'},
-        span(
-          { className: 'switch' },
-          input({
-            className: 'switch-input',
-            id: 'enabledSwitch',
-            type: 'checkbox',
-            name: 'enabledSwitch',
-            autoComplete: 'off',
-            checked: disabled ? null : true
-          }),
-          label(
-            {
-              className: 'switch-paddle',
-              id: 'toggle-blok',
-              htmlFor: 'enabledSwitch',
-              onClick: sendToggleMessage
-            },
-            span(
-              { className: 'show-for-sr' },
-              'Toggle Blok'
-            )
-          )
-        )
-      )
+    return (
+      <div className='row align-middle'>
+        <div className='small-8 columns toggle-label-column'>
+          {labelText}
+        </div>
+        <div className='small-4 columns toggle-column'>
+          <span className='switch'>
+            <input className='switch-input' id='enabledSwitch' type='checkbox' name='enabledSwitch' autoComplete='off' checked={checked} />
+            <label className='switch-paddle' id='toggle-blok' htmlFor='enabledSwitch' onClick={sendToggleMessage}>
+              <span className='show-for-sr'>
+                'Toggle Blok'
+              </span>
+            </label>
+          </span>
+        </div>
+      </div>
     )
   }
 }))
 
-const MainPanel = createFactory(createClass({
+const MainPanel = React.createFactory(React.createClass({
   render: function () {
     let {disabled, hostReport} = this.props
+    let feedback = null
 
-    let title = div({className: 'row'},
-      Title({ disabled: disabled })
+    let title = (
+      <div className='row'>
+        <Title disabled={disabled} />
+      </div>
     )
-    let report = div({className: 'row'},
-      HostReport({ hostReport: hostReport })
+    let report = (
+      <div className='row'>
+        <HostReport hostReport={hostReport} />
+      </div>
     )
-    let feedback = disabled ? null : div({ className: 'row align-center' }, Feedback({ showFeedbackPanel: this.props.showFeedbackPanel }))
-    let toggle = Toggle({ disabled: disabled, sendToggleMessage: sendToggleMessage })
-    return div(
-      {id: 'main-panel'},
-      title, report, feedback, hr(), toggle
+    if (!disabled) {
+      feedback = (
+        <div className='row align-center'>
+          <Feedback showFeedbackPanel={this.props.showFeedbackPanel} />
+        </div>
+      )
+    }
+    let toggle = (
+      <Toggle disabled={disabled} sendToggleMessage={sendToggleMessage} />
+    )
+    return (
+      <div id='main-panel'>
+        {title}
+        {report}
+        {feedback}
+        <hr />
+        {toggle}
+      </div>
     )
   }
 }))
 
-const FeedbackBreakage = createFactory(createClass({
+const FeedbackBreakage = React.createFactory(React.createClass({
   render: function () {
     let breakage = this.props.breakage
-    return div({},
-      input(
-        {className: 'breakage', type: 'radio', name: 'breakage', value: breakage, id: breakage}
-      ),
-      label(
-        {htmlFor: breakage},
-        breakage
-      )
+    return (
+      <div>
+        <input className='breakage' type='radio' name='breakage' value={breakage} id={breakage} />
+        <label htmlFor={breakage}>
+          {breakage}
+        </label>
+      </div>
     )
   }
 }))
 
-const FeedbackPanel = createClass({
+const FeedbackPanel = React.createClass({
   render: function () {
-    return div(
-      {id: 'feedback-panel'},
-      div(
-        {className: 'row panel-row'},
-        div(
-          {
-            className: 'small-1 columns feedback-panel-back-arrow',
-            onClick: this.props.showMainPanel
-          },
-          '\u2329'
-        ),
-        div(
-          {className: 'small-11 columns feedback-panel-form'},
-          div(
-            {className: 'row'},
-            h5(
-              {},
-              'Thanks! We have noted you saw a problem with this page.'
-            )
-          ),
-          div(
-            {className: 'row'},
-            p(
-              {},
-              'Please share any additional information about the problems you see.'
-            ),
-            fieldset(
-              {},
-              legend(
-                {},
-                'I noticed a problem with:'
-              ),
-              FeedbackBreakage({ breakage: 'images' }),
-              FeedbackBreakage({ breakage: 'video' }),
-              FeedbackBreakage({ breakage: 'layout' }),
-              FeedbackBreakage({ breakage: 'buttons' }),
-              FeedbackBreakage({ breakage: 'other' })
-            )
-          ),
-          div(
-            {className: 'row'},
-            label(
-              {className: 'notes-label'},
-              'Why do you think Blok caused this problem?',
-              textarea(
-                {id: 'notes', rows: 3}
-              )
-            )
-          ),
-          div(
-            {className: 'row'},
-            a(
-              {
-                id: 'submit-btn',
-                className: 'expanded button',
-                onClick: this.props.sendBreakageMessage
-              },
-              'Submit'
-            )
-          )
-        )
-      )
+    return (
+      <div id='feedback-panel'>
+        <div className='row panel-row'>
+          <div className='small-1 columns feedback-panel-back-arrow' onClick={this.props.showMainPanel}>
+            &lang;
+          </div>
+          <div className='small-11 columns feedback-panel-form'>
+            <div className='row'>
+              <h5>Thanks! We have noted you saw a problem with this page.</h5>
+            </div>
+            <div className='row'>
+              <p>Please share any additional information about the problems you see.</p>
+              <fieldset>
+                <legend>I noticed a problem with:</legend>
+                <FeedbackBreakage breakage='images' />
+                <FeedbackBreakage breakage='video' />
+                <FeedbackBreakage breakage='layout' />
+                <FeedbackBreakage breakage='buttons' />
+                <FeedbackBreakage breakage='other' />
+              </fieldset>
+            </div>
+            <div className='row'>
+              <label className='notes-label'>
+                Why do you think Blok caused this problem?
+                <textarea id='notes' rows='3' />
+              </label>
+            </div>
+            <div className='row'>
+              <a id='submit-btn' className='expanded button' onClick={this.props.sendBreakageMessage}>Submit</a>
+            </div>
+          </div>
+        </div>
+      </div>
     )
   }
 })
 
-const Popup = createClass({
+const Popup = React.createClass({
   getInitialState: function () {
     return { showFeedbackPanel: false }
   },
@@ -256,28 +205,16 @@ const Popup = createClass({
 
   render: function () {
     let {disabled, hostReport, sendToggleMessage} = this.props
-    let mainPanel = null
-    let feedbackPanel = null
 
     if (this.state.showFeedbackPanel) {
-      feedbackPanel = createElement(FeedbackPanel,
-        {
-          showMainPanel: this.showMainPanel,
-          sendBreakageMessage: this.props.sendBreakageMessage
-        }
+      return (
+        <FeedbackPanel showMainPanel={this.showMainPanel} sendBreakageMessage={this.props.sendBreakageMessage} />
       )
     } else {
-      mainPanel = createElement(MainPanel,
-        {
-          disabled: disabled,
-          hostReport: hostReport,
-          sendToggleMessage: sendToggleMessage,
-          showFeedbackPanel: this.showFeedbackPanel
-        }
+      return (
+        <MainPanel disabled={disabled} hostReport={hostReport} sendToggleMessage={sendToggleMessage} showFeedbackPanel={this.showFeedbackPanel} />
       )
     }
-
-    return div({}, mainPanel, feedbackPanel)
   }
 })
 
@@ -309,14 +246,7 @@ browser.runtime.getBackgroundPage((bgPage) => {
   let pageHostReport = bgPage.topFrameHostReport
 
   ReactDOM.render(
-    createElement(Popup,
-      {
-        disabled: disabled,
-        hostReport: pageHostReport,
-        sendToggleMessage: sendToggleMessage,
-        sendBreakageMessage: sendBreakageMessage
-      }
-    ),
+    <Popup disabled={disabled} hostReport={pageHostReport} sendToggleMessage={sendToggleMessage} sendBreakageMessage={sendBreakageMessage} />,
     document.getElementById('popup')
   )
 })
