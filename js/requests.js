ee87aee61fb8a12fdd08636b185acd149db81eb9
groovecoder <luke.crouch@gmail.com>
2016-09-07 11:28:02 -0500

    for #132: memo-ize hostEntity when possible

--- js/requests.js
+++ js/requests.js
@@ -1,14 +1,32 @@
 const {log} = require('./log')
 const {hostInEntity} = require('./lists')
 
+let hostEntityCache = {}
+
 function requestAllower (tabID, totalExecTime, startDateTime) {
   totalExecTime[tabID] += Date.now() - startDateTime
   return {}
 }
 
 function getRequestEntity (entityList, originTopHost, requestTopHost, mainFrameOriginTopHost) {
-  let requestEntityName = null
-  let sameEntity = false
+  let requestEntity = {'entityName': null, 'sameEntity': false}
+
+  // First, try to return everything from memo-ized cache
+  let requestEntityName = hostEntityCache[requestTopHost]
+  let originEntityName = hostEntityCache[originTopHost]
+  let mainFrameOriginEntityName = hostEntityCache[mainFrameOriginTopHost]
+  requestEntity.sameEntity = (
+    requestEntityName && (
+      requestEntityName === originEntityName || requestEntityName === mainFrameOriginEntityName
+    )
+  )
+  if (requestEntity.sameEntity) {
+    requestEntity.entityName = requestEntityName
+    log('returning from memo-ized cache: ', requestEntity)
+    return requestEntity
+  }
+
+  // If a host was not found in the memo-ized cache, check thru the entityList
   for (let entityName in entityList) {
     let entity = entityList[entityName]
     let requestIsEntityResource = false
@@ -17,21 +35,28 @@ function getRequestEntity (entityList, originTopHost, requestTopHost, mainFrameO
 
     requestIsEntityResource = hostInEntity(entity.resources, requestTopHost)
     if (requestIsEntityResource) {
-      requestEntityName = entityName
+      requestEntity.entityName = entityName
+      hostEntityCache[requestTopHost] = entityName
     }
 
     originIsEntityProperty = hostInEntity(entity.properties, originTopHost)
+    if (originIsEntityProperty) {
+      hostEntityCache[originTopHost] = entityName
+    }
 
     mainFrameOriginIsEntityProperty = hostInEntity(entity.properties, mainFrameOriginTopHost)
+    if (mainFrameOriginIsEntityProperty) {
+      hostEntityCache[mainFrameOriginTopHost] = entityName
+    }
 
     if ((originIsEntityProperty || mainFrameOriginIsEntityProperty) && requestIsEntityResource) {
       log(`originTopHost ${originTopHost} and resource requestTopHost ${requestTopHost} belong to the same entity: ${entityName}; allowing request`)
-      sameEntity = true
+      requestEntity.sameEntity = true
       break
     }
   }
   // TODO: https://github.com/mozilla/blok/issues/110
-  return {'entityName': requestEntityName, 'sameEntity': sameEntity}
+  return requestEntity
 }
 
 module.exports = {
